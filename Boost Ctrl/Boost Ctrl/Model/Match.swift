d0c8d824dfd54499da5edf368b1a689b3c3484cd
Zac Johnson <zacjohnson@aggiemail.usu.edu>
2018-07-13 02:49:27 -0600

    Huge commit (whoops). Completed Teams and Standings pages as well as Links page.

---
+++ Boost Ctrl/Boost Ctrl/Model/Match.swift
@@ -0,0 +1,16 @@
+//
+//  Match.swift
+//  Boost ctRL
+//
+//  Created by Zac Johnson on 7/13/18.
+//  Copyright © 2018 Zac Johnson. All rights reserved.
+//
+
+import Foundation
+
+//class Match {
+//	let teamOne: String
+//	let teamTwo: String
+//	let scoreOne: String
+//	let scoreTwo: String
+//}
