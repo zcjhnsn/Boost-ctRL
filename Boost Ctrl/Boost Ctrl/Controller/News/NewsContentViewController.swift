b70c18d0f52777118b83ac7b30ec612e068dce12
Zac Johnson <zac@lightningkite.com>
2019-07-22 23:03:51 -0600

    No more force unwrapping news objects from firebase.

--- Boost Ctrl/Boost Ctrl/Controller/News/NewsContentViewController.swift
+++ Boost Ctrl/Boost Ctrl/Controller/News/NewsContentViewController.swift
@@ -105,11 +105,11 @@ class NewsContentViewController: UIViewController {
 			
 			let snapshotValue = snapshot.value as! Dictionary<String, String>
 			
-			let headline = snapshotValue["h"]!
-			let detail = snapshotValue["d"]!
-			let categoryString = snapshotValue["c"]!
-			let link = snapshotValue["l"]!
-			let siteName = snapshotValue["s"]!
+            let headline = snapshotValue["h"] ?? "No headline provided"
+            let detail = snapshotValue["d"] ?? "No details provided."
+            let categoryString = snapshotValue["c"] ?? "news"
+            let link = snapshotValue["l"] ?? "https://twitter.com/boostctrl"
+            let siteName = snapshotValue["s"] ?? "Twitter"
 			
 			let news = News()
 			news.headline = headline
