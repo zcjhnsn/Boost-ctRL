f5ca33cb8e18b40b9c2f4f935658d2fa7a83715f
Zac Johnson <zac@lightningkite.com>
2021-03-13 20:51:27 -0700

    Initial Commit

---
+++ Boost ctRL/Boost_ctRLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boost_ctRLApp.swift
+//  Boost ctRL
+//
+//  Created by Zac Johnson on 3/13/21.
+//
+
+import SwiftUI
+
+@main
+struct Boost_ctRLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
